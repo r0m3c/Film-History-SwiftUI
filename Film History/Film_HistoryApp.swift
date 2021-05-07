7479416e040f6aa0451dcd99d3c3ff9311c15b60
r0m3c <leandrogamarra@rocketmail.com>
2021-05-07 00:53:48 -0400

    Initial Commit

---
+++ Film History/Film_HistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Film_HistoryApp.swift
+//  Film History
+//
+//  Created by Leandro Gamarra on 5/7/21.
+//
+
+import SwiftUI
+
+@main
+struct Film_HistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
